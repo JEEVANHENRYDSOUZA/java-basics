5dfcbc6779b02da431ebb56158644982e8ef4953
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-11 18:26:18 -0400

    Create taking user input in java.java

---
+++ taking user input in java.java
@@ -0,0 +1,6 @@
+Scanner sc = new Scanner(System.in);  // Create a Scanner object
+ System.out.println("Enter your age");
+
+	    int age = sc.nextInt();  // Read user input
+	    System.out.println(age);
+          
