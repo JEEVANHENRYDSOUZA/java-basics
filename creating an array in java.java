5239fbc32bc91720bf52a2965baa8d5f3a44624a
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-11 18:29:50 -0400

    Create creating an array in java.java

---
+++ creating an array in java.java
@@ -0,0 +1,38 @@
+//.java extension for java program 
+//execution starts from main always
+package helloworld;
+import java.util.Scanner;
+
+public class hello {
+
+	public static void  main(String[] args) {
+		//string is a non-primitive datatype
+		//primitive original
+         //int age=34;
+         //sysout ctrl + space          
+           
+         // System.out.println("welcome to java ");//given some output to the console
+          //printing a string inside system.out.println
+          
+              // Scanner sc=new Scanner(System.in);           
+        		  // int age =sc.nextInt();
+			//i//nt age=10;
+        		   //System.out.println(age);
+		
+		
+		//Scanner sc = new Scanner(System.in);  // Create a Scanner object
+	    //System.out.println("Enter your age");
+
+	    //int age = sc.nextInt();  // Read user input
+	    System.out.println("creating an array in java");
+	    //memory is continuosly allocated in java
+	    //they store similar type of data
+	    //creating an array in java
+	    int[] marks= new int[10];
+	    marks[0]=1000;
+	    System.out.println(marks[0]);
+	    
+          
+	}
+
+}
