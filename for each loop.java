918054ced682e8dd0349f29d9101ddb521e12974
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-17 14:13:03 -0400

    Create for each loop.java

---
+++ for each loop.java
@@ -0,0 +1,9 @@
+    int [] age= new int[100];
+	    age[0]=1;
+	    System.out.println(age[0]);
+	    //age has the refernce to the integer array
+//foreachloop
+	    
+	    for(int element:age) {System.out.println(element);}
+
+//b default integer  array values are intiailized to zero
