2712d32c6d6945f199971d6a19746e35d9719fa8
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-07-13 18:43:12 -0400

    Create binary search .java

---
+++ binary search/binary search .java
@@ -0,0 +1,60 @@
+public class binarysearch {
+    static void binarysearch(int array[],int key){
+
+        int left=0;
+        int right=array.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(array[mid]==key){
+                System.out.println("match found");
+                return;
+
+            }
+            else if(array[mid]<key){
+                left=mid;
+            }
+            else{
+                right=mid;
+            }
+        }
+        System.out.println("match not found");
+        return;
+
+
+    }
+    public static void main(String[] args){
+
+
+        System.out.println("binary search");
+
+        //conditions for binary search array should be sorted
+        //time complexity o(log(n))
+        //two variations of it 
+        //1.recursive
+        //2.iterative
+        //very important for interviews
+        //problems should have terms such as key,value,sorted
+        //psuedo code
+        //find mid
+        //take two pointers left and right
+        //left should be equal to the first element in the array
+        //right should be equal to the last element in the array
+        //return -1 when element is not found
+        //iterate till left is less than right
+        //if we have not found then the value which the left points to it is the first closest element to the key which we are looking for
+        //calculate mid-value as l+(r-1)/2
+        //coding binary search
+        //this code is only for non-repating elements in the array
+        int array[]={1,2,3,4,5,6,7,8,9};
+        binarysearch(array,4);
+
+
+
+
+
+    }
+
+
+
+
+}
